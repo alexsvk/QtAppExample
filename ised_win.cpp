ad91fdf46a8331bff941cd3c13ed8e2cfa74595f
alexsvk <alexander.svk@gmail.com>
2023-04-06 16:44:14 +0300

    Add files via upload
    Asynchonous behavior was added

--- ised_win.cpp
+++ ised_win.cpp
@@ -22,7 +22,8 @@
 #include <QDialog>
 #include <QTextEdit>
 #include <qdebug.h>
-#include <memory>
+#include <future>
+#include <chrono>
 // LOCAL
 #include "ised_win.h" // IMPLEMENTS
 
@@ -67,14 +68,10 @@ void ised_win::accept()
 {
     m_session = m_edit->text();
 
-    int c = calc();
-    // Add
-    if (grid)
-    {
-        //delete grid;
-    }
+    this->calculateCountofWidgetsAsync();
+
     grid = new QVBoxLayout();
-    for (int j = 0; j < c; ++j)
+    for (int j = 0; j < m_countOfWidgets; ++j)
     {
         char buffer[3];
         itoa(j, buffer, 10);//to do
@@ -102,8 +99,8 @@ void ised_win::onBtnPressed()
     ndlg->setModal(false);
     ndlg->resize(320, 240);
     QVBoxLayout* l = new QVBoxLayout;
-    int c = calc();
-    for (int j = 0; j < c; ++j)
+
+    for (int j = 0; j < m_countOfWidgets; ++j)
     {
         char buffer[3];
         itoa(j, buffer, 10);//to do
@@ -114,6 +111,7 @@ void ised_win::onBtnPressed()
     ndlg->show();
 }//onBtnPressed
 
+// WE CALL THESE METHODS ASYNCHRONOUSLY
 int ised_win::calc()
 {
     int c = 0;
@@ -130,5 +128,23 @@ int ised_win::calc()
         c = 2;
     }
     return c;
+}
+
+void ised_win::calculateCountofWidgetsAsync()
+{
+    auto fut = std::async(std::mem_fun(calc), this);
+    if (fut.wait_for(std::chrono::milliseconds(0)) == std::future_status::deferred)
+    {
+        // Task is deferred => call it asynchronously
+        m_countOfWidgets = fut.get();
+    }
+    else
+        while(fut.wait_for(std::chrono::milliseconds(100)) != std::future_status::ready)
+        {
+            // The task isn't ready yet, and the endless cycle is impossible
+            // do whatever you wish parallely
+            qDebug("fut.wait_for(std::chrono::milliseconds(100)\n");
+        }
+    m_countOfWidgets = fut.get();
 }
 }
